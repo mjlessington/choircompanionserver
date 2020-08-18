bdbc509d2432f83eaf9af7881e0917b623051eec
Mell Lessington <59120468+mjlessington@users.noreply.github.com>
2020-08-18 14:21:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + JSON.stringify(msg));
+    io.emit('chat message', msg);
+  });
+});
+
+
+
+http.listen(port, () => {
+  console.log('listening on *:3001');
+});
